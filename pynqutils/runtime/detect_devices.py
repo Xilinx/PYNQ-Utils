20edadee7c51db8112f13dd1b89e8e09107daa59
sfleming <sfleming@xilinx.com>
2022-11-09 10:34:28 +0000

    prevent erroring out if xbuitl is not installed when trying to detect the list of devices

--- pynqutils/runtime/detect_devices.py
+++ pynqutils/runtime/detect_devices.py
@@ -6,11 +6,14 @@ import re
 
 def detect_devices():
     """ Returns a list of devices """
-    examine_str = str(subprocess.check_output("xbutil examine", shell=True))
-    pattern = re.compile("\[[0-9]+:[0-9]+:[0-9]+\.[0-9]+]\s*:\s+([A-Za-z0-9_-]+)\s+")
-    devices = []
-    for match in pattern.finditer(examine_str):
-        devices.append(match.group(1))
+    try: # xbutil might not be installed on the machine, in that case return an empty list of devices
+        examine_str = str(subprocess.check_output("xbutil examine", shell=True))
+        pattern = re.compile("\[[0-9]+:[0-9]+:[0-9]+\.[0-9]+]\s*:\s+([A-Za-z0-9_-]+)\s+")
+        devices = []
+        for match in pattern.finditer(examine_str):
+            devices.append(match.group(1))
+    except:
+        devices = []
     return devices
 
 
