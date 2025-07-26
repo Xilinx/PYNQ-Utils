2c0e0283acb3ddcb3d4092ac8cc5813a78c73b70
Josh Goldsmith <28935218+jogomojo@users.noreply.github.com>
2025-07-26 14:33:01 +0100

    adding remote logic (#12)

--- pynqutils/setup_utils/get_platform.py
+++ pynqutils/setup_utils/get_platform.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import platform
-
+import os
 
 class UnsupportedPlatform(Exception):
     """Exception raised when an unsupported platform is attempted to be used"""
@@ -11,11 +11,11 @@ class UnsupportedPlatform(Exception):
 
 
 def get_platform() -> str:
-    """Get the current platform, either edge or pcie"""
-    cpu = platform.processor()
-    if cpu in ["armv7l", "aarch64"]:
+    """Get the current platform"""
+    # Detect if we are running on a PYNQ board running Classic PYNQ
+    on_target = os.path.isfile('/proc/device-tree/chosen/pynq_board')
+
+    if on_target or os.getenv("BOARD"):
         return "edge"
-    elif cpu in ["x86_64"]:
-        return "pcie"
     else:
         raise UnsupportedPlatform("Platform with cpu {cpu} is not supported.")
